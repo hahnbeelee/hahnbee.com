d6b20aeb7cc8fc3f064991a9b0b56f9f3bdc9d4c
Hahnbee Lee <55263191+hahnbeelee@users.noreply.github.com>
2025-04-28 10:42:51 -0700

    simple

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Hahnbee Lee
-        <div className="text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          Co-founder @ Mintlify
-        </div>
-      </main>
-    </div>
+    <main className="p-6 pt-3 md:pt-6 min-h-screen">
+      <header className="mb-3 font-bold">Hahnbee Lee</header>
+      <div className="text-sm/6 font-[family-name:var(--font-geist-mono)]">
+        Co-founder @ Mintlify
+      </div>
+    </main>
   );
 }
